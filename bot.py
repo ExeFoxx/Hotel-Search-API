6328e3fdeccdaa698656cd707e1f34fa86d9638c
bandr <bandriy21@gmail.com>
2024-05-25 15:16:22 +0200

    test

--- bot.py
+++ bot.py
@@ -2,38 +2,11 @@ import telebot
 from telebot import types
 import logging
 from config import TOKEN
-
-import requests
-
-def send_message_to_user(text, user_id, TOKEN):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    payload = {
-        'chat_id': user_id,
-        'text': text,
-        'parse_mode': 'HTML'
-    }
-    response = requests.post(url, data=payload)
-    return response.json()
-
-def send_rental_orders_to_user(user_id, TOKEN):
-    try:
-        with open('rental_orders.txt', 'r', encoding='utf-8') as f:
-            orders = f.read()
-            send_message_to_user(orders, user_id, TOKEN)
-    except FileNotFoundError:
-        send_message_to_user("Файл заказов на аренду не найден.", user_id, TOKEN)
-    except Exception as e:
-        send_message_to_user(f"Произошла ошибка: {e}", user_id, TOKEN)
-
-# Пример использования:
-# ВАЖНО: Замените 'YOUR_TELEGRAM_USER_ID' на ваш идентификатор пользователя в Telegram
-# и 'YOUR_TELEGRAM_BOT_TOKEN' на токен вашего бота.
-send_rental_orders_to_user('866323263', '7065295403:AAE9KQZwaliZkNwJDCaYZ_lan04yysr4Sxw')
-
-bot = telebot.TeleBot(TOKEN)
-
 from CAR import CAR_BRANDS
 from CITIES import CITIES
+
+
+bot = telebot.TeleBot(TOKEN)
 
 DELIVERY_TIMES = [f"{hour}:{minute}" for hour in range(8, 21) for minute in ('00', '30')]
 
@@ -53,9 +26,9 @@ STATE_SURNAME = 2
 STATE_PASSPORT_NUMBER = 3
 STATE_DRIVER_LICENSE = 4
 STATE_CONTACT = 5
+DELIVERY_TIME = 6
 
 user_data = {}
-
 user_states = {}
 
 def get_user_state(user_id):
@@ -67,6 +40,7 @@ def update_user_state(user_id, state):
 def clear_user_state(user_id):
     user_states.pop(user_id, None)
 
+# Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def welcome(message):
     try:
@@ -88,6 +62,23 @@ def welcome(message):
     except Exception as e:
         logging.error(f"Произошла ошибка: {type(e).__name__}, {str(e)}")
 
+bot.message_handler(func=lambda message: message.text in DELIVERY_TIMES)
+def process_delivery_time(message):
+    if get_user_state(message.from_user.id) == DELIVERY_TIME:
+        selected_time = message.text
+        user_data[message.from_user.id]['delivery_time'] = selected_time
+
+        # Записываем выбранное время в файл
+        with open('delivery_times.txt', 'a') as file:
+            file.write(selected_time + '\n')
+
+        # Очищаем состояние пользователя
+        clear_user_state(message.from_user.id)
+
+        bot.send_message(message.chat.id, f"Выбранное время доставки: {selected_time}")
+    else:
+        bot.send_message(message.chat.id, "Ошибка: выберите время доставки снова.")
+
 @bot.message_handler(func=lambda message: message.text == 'Подтвердить')
 def ask_for_name(message):
     update_user_state(message.from_user.id, STATE_NAME)
@@ -96,40 +87,95 @@ def ask_for_name(message):
 
 def process_name_step(message):
     if get_user_state(message.from_user.id) == STATE_NAME:
-        user_data[message.from_user.id] = {'name': message.text}
-        update_user_state(message.from_user.id, STATE_SURNAME)
-        msg = bot.send_message(message.chat.id, "Укажите Фамилию:")
-        bot.register_next_step_handler(msg, process_surname_step)
+        if message.text.strip() and message.text != 'Подтвердить':
+            user_data[message.from_user.id] = {'name': message.text}
+            update_user_state(message.from_user.id, STATE_SURNAME)
+            msg = bot.send_message(message.chat.id, "Укажите Фамилию:")
+            bot.register_next_step_handler(msg, process_surname_step)
+        else:
+            msg = bot.send_message(message.chat.id, "Ошибка: введите имя.")
+            bot.register_next_step_handler(msg, process_name_step)
 
 def process_surname_step(message):
     if get_user_state(message.from_user.id) == STATE_SURNAME:
-        user_data[message.from_user.id]['surname'] = message.text
-        update_user_state(message.from_user.id, STATE_PASSPORT_NUMBER)
-        msg = bot.send_message(message.chat.id, "Укажите Номер Паспорта:")
-        bot.register_next_step_handler(msg, process_passport_step)
+        if message.text.strip() and message.text != 'Подтвердить':
+            user_data[message.from_user.id]['surname'] = message.text
+            update_user_state(message.from_user.id, STATE_PASSPORT_NUMBER)
+            msg = bot.send_message(message.chat.id, "Укажите Номер Паспорта:")
+            bot.register_next_step_handler(msg, process_passport_step)
+        else:
+            msg = bot.send_message(message.chat.id, "Ошибка: введите фамилию.")
+            bot.register_next_step_handler(msg, process_surname_step)
 
 def process_passport_step(message):
     if get_user_state(message.from_user.id) == STATE_PASSPORT_NUMBER:
-        user_data[message.from_user.id]['passport_number'] = message.text
-        update_user_state(message.from_user.id, STATE_DRIVER_LICENSE)
-        msg = bot.send_message(message.chat.id, "Укажите номер водительских прав:")
-        bot.register_next_step_handler(msg, process_driver_license_step)
+        if message.text.strip() and message.text != 'Подтвердить':
+            user_data[message.from_user.id]['passport_number'] = message.text
+            update_user_state(message.from_user.id, STATE_DRIVER_LICENSE)
+            msg = bot.send_message(message.chat.id, "Укажите номер водительских прав:")
+            bot.register_next_step_handler(msg, process_driver_license_step)
+        else:
+            msg = bot.send_message(message.chat.id, "Ошибка: введите номер паспорта.")
+            bot.register_next_step_handler(msg, process_passport_step)
 
 def process_driver_license_step(message):
     if get_user_state(message.from_user.id) == STATE_DRIVER_LICENSE:
-        user_data[message.from_user.id]['driver_license'] = message.text
-        update_user_state(message.from_user.id, STATE_CONTACT)
-        msg = bot.send_message(message.chat.id, "Укажите мессенджер для связи или местный номер:")
-        bot.register_next_step_handler(msg, process_contact_step)
+        if message.text.strip() and message.text != 'Подтвердить':
+            user_data[message.from_user.id]['driver_license'] = message.text
+            update_user_state(message.from_user.id, STATE_CONTACT)
+            msg = bot.send_message(message.chat.id, "Укажите мессенджер для связи или местный номер:")
+            bot.register_next_step_handler(msg, process_contact_step)
+        else:
+            msg = bot.send_message(message.chat.id, "Ошибка: введите номер водительских прав.")
+            bot.register_next_step_handler(msg, process_driver_license_step)
 
 def process_contact_step(message):
     if get_user_state(message.from_user.id) == STATE_CONTACT:
-        user_data[message.from_user.id]['contact'] = message.text
+        if message.text.strip() and message.text != 'Подтвердить':
+            user_data[message.from_user.id]['contact'] = message.text
+            update_user_state(message.from_user.id, DELIVERY_TIME)
+            msg = bot.send_message(message.chat.id, "Выберите время:")
+            bot.register_next_step_handler(msg, process_time_step)
+        else:
+            msg = bot.send_message(message.chat.id, "Ошибка: введите контактные данные.")
+            bot.register_next_step_handler(msg, process_contact_step)
+
+def process_time_step(message):
+    if get_user_state(message.from_user.id) == DELIVERY_TIME:
+        if message.text.strip() and message.text not in ['Подтвердить', 'Пропустить']:
+            user_data[message.from_user.id]['time'] = message.text
+            # Записываем выбранное время в файл
+            with open('delivery_times.txt', 'a') as file:
+                file.write(f"User ID {message.from_user.id} selected time: {message.text}\n")
+        elif message.text == 'Пропустить':
+            user_data[message.from_user.id]['time'] = 'Не указано'
+        else:
+            msg = bot.send_message(message.chat.id, "Ошибка: выберите время или нажмите 'Пропустить'.")
+            bot.register_next_step_handler(msg, process_time_step)
+            return
         bot.send_message(message.chat.id, "Спасибо, ваш заказ принят.")
         save_rental_info(user_data[message.from_user.id])
         clear_user_state(message.from_user.id)
         user_data.pop(message.from_user.id, None)
 
+def save_rental_info(rental_info):
+    try:
+        with open('rental_orders.txt', 'a', encoding='utf-8') as f:
+            f.write(str(rental_info) + '\n')
+    except Exception as e:
+        logging.error(f"Ошибка при сохранении информации о заказе: {type(e).__name__}, {str(e)}")
+
+def process_contact_step(message):
+    if get_user_state(message.from_user.id) == STATE_CONTACT:
+        if message.text:
+            user_data[message.from_user.id]['contact'] = message.text
+            bot.send_message(message.chat.id, "Спасибо, ваш заказ принят.")
+            save_rental_info(user_data[message.from_user.id])
+            clear_user_state(message.from_user.id)
+            user_data.pop(message.from_user.id, None)
+        else:
+            msg = bot.send_message(message.chat.id, "Ошибка: введите контактные данные.")
+            bot.register_next_step_handler(msg, process_contact_step)
 
 def save_rental_info(rental_info):
     try:
@@ -196,5 +242,6 @@ def lalala(message):
                 send_back_button(message.chat.id, "Пожалуйста, используйте кнопки для навигации.")
     except Exception as e:
         logging.error(f"Произошла ошибка: {type(e).__name__}, {str(e)}")
+
 
 bot.polling(none_stop=True)
