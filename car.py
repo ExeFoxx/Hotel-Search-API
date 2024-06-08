8690d3e0ae6098dea2995f115262dd67b01daf2c
Andriy Bodnar <bandriy21@gmail.com>
2024-06-08 23:24:24 +0300

    Delete car.py

--- car.py
+++
@@ -1,7 +0,0 @@
-car_models = {
-    "Toyota": ["Corolla", "Camry", "RAV4", "Highlander"],
-    "Ford": ["Focus", "Fusion", "Mustang", "Explorer"],
-    "Honda": ["Civic", "Accord", "CR-V", "Pilot"],
-    "Volkswagen": ["Golf", "Jetta", "Passat", "Tiguan"]
-
-}
